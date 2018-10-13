57d5ea00687b588528e2e4dc4274bd2835c5c459
vigyat123 <vigyattripathi@gmail.com>
2018-10-14 02:02:21 +0530

    Switching between activities

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.smartherd.msgshareapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -14,7 +15,14 @@ class MainActivity : AppCompatActivity() {
 
         btnShowToast.setOnClickListener {
             Log.i("MainActivity", "Button was clicked")
-            Toast.makeText(this, "Button was clicked", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Button was clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
