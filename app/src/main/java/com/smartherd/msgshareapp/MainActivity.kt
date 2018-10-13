a475ec33cb29f3ae53a552e489e33c577c6efc2d
vigyat123 <vigyattripathi@gmail.com>
2018-10-14 03:01:28 +0530

    Sharing messagee to other apps like whatsApp

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -24,5 +24,14 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("user_message", message)
             startActivity(intent)
         }
+
+        btnShareToOtherApps.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, "Share To"))
+        }
     }
 }
