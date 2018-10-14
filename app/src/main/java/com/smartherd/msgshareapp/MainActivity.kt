d90c16b2fcc30a30282b91c435c807e71eeb4182
vigyat123 <vigyattripathi@gmail.com>
2018-10-14 15:02:54 +0530

    Section 3: Implement ListView

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -33,5 +33,10 @@ class MainActivity : AppCompatActivity() {
             intent.type = "text/plain"
             startActivity(Intent.createChooser(intent, "Share To"))
         }
+
+        btnRecyclerViewDemo.setOnClickListener {
+            val intent = Intent(this, HobbiesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
